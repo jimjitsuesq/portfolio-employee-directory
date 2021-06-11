939c35e43f524e559adf3a267372a3451c1f39d4
Jim Peters <jimjitsuesq@gmail.com>
2021-06-10 23:06:57 -0700

    Cleanup
    Cleaned up code and refactored

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,27 @@ const gallery = document.getElementById('gallery');
 const cardInfo = document.querySelector('.card-info-container');
 const modalDiv = document.querySelector('.modal');
 const modalContainer = document.querySelector('.modal-container');
-//const modalInfoContainer = document.querySelector('.modal-info-container');
+const modalPrev = document.getElementById('modal-prev');
+const modalNext = document.getElementById('modal-next');
 const searchInput = document.getElementById('search-input');
 const searchSubmit = document.getElementById('search-submit');
-let employeeCards = [];
-let employeeModals = [];
+const employeeCards = [];
+const employeeModals = [];
 let currentCardId;
 
 modalContainer.style.display = 'none';
 searchSubmit.style.display = 'none';
-function generateCardsModals (data) {
+
+async function fetchEmployeeData () {
+    const response = await fetch('https://randomuser.me/api/?results=12&nat=US&noinfo')
+    await response.json()
+    .then((result) => {
+        generateHTML(result)
+    })
+}
+fetchEmployeeData();
+
+function generateHTML (data) {
     data.results.map( (person, index) => {
         const card = document.createElement('div');
         card.insertAdjacentHTML('beforeend', `
@@ -28,96 +39,57 @@ function generateCardsModals (data) {
         `);
         employeeCards.push(card);
     });
-    data.results.map( (person, index) => {
-        
-        const fixedPhone = person.cell.replace("-", " ")
-        const birthYear = person.dob.date.slice(0, 4)
-        const birthMonth = person.dob.date.slice(5, 7)
-        const birthDay = person.dob.date.slice(8, 10)
-
+    data.results.map( (person) => {
+        const fixedPhone = person.cell.replace("-", " ");
+        const birthYear = person.dob.date.slice(0, 4);
+        const birthMonth = person.dob.date.slice(5, 7);
+        const birthDay = person.dob.date.slice(8, 10);
         let html = 
-        //modal.insertAdjacentHTML('beforeend', 
-            //<div class="modal">
-            //<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-            //<div class="modal-info-container" id=${index}>
-                `<img class="modal-img" src="${person.picture.large}" id=${index} alt="profile picture">
-                <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
-                <p class="modal-text">${person.email}</p>
-                <p class="modal-text cap">${person.location.city}</p>
-                <hr>
-                <p class="modal-text">${fixedPhone}</p>
-                <p class="modal-text">123 Portland Ave., ${person.location.city}, ${person.location.state} ${person.location.postcode}</p>
-                <p class="modal-text">Birthday: ${birthMonth}/${birthDay}/${birthYear}</p>`
-            //</div>
-        //);
+            `<img class="modal-img" src="${person.picture.large}" alt="profile picture">
+            <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
+            <p class="modal-text">${person.email}</p>
+            <p class="modal-text cap">${person.location.city}</p>
+            <hr>
+            <p class="modal-text">${fixedPhone}</p>
+            <p class="modal-text">123 Portland Ave., ${person.location.city}, ${person.location.state} ${person.location.postcode}</p>
+            <p class="modal-text">Birthday: ${birthMonth}/${birthDay}/${birthYear}</p>`;
         employeeModals.push(html);
-        });
+    });
     displayemployeeCards();
-    
 }
-
-/* function listener (event) {
-    cardClick = event.target;
-            findID(cardClick);
-            displayModal(ModalId, employeeModals);
-            e.stopPropagation;
-} */
 
 function displayemployeeCards () {
     for (let i=0; i<employeeCards.length; i++) {
-        gallery.appendChild(employeeCards[i]);    
-    }
-    addModalListener();
-}
-
-function addModalListener () {
-    const currentCards = document.getElementsByClassName('card');
-    for (let i=0; i<currentCards.length; i++) {
-        currentCards[i].addEventListener('click', (e) => {
-            cardClickTarget = e.target;
-            cardClickTargetId = cardClickTarget.closest('.card').id;
-            displayModal(cardClickTargetId);
-            e.stopPropagation;
+        gallery.appendChild(employeeCards[i]);
+        employeeCards[i].addEventListener('click', (e) => {
+            cardClickedId = e.target.closest('.card').id;
+            displayModal(cardClickedId);
+            e.stopPropagation;   
         });
     }
 }
 
-function displayModal (cardClickTargetId) {
+function displayModal (cardClickedId) {
+    const currentCards = [...(document.getElementsByClassName('card'))];
     const modalCloseBtn = document.getElementById('modal-close-btn');
     const modal = document.createElement('div');
-    modal.id = cardClickTargetId;
-    currentCardId = cardClickTargetId
+    modal.id = cardClickedId;
     modalContainer.style.display = 'block';
     modal.className = ('modal-info-container');
-    modal.innerHTML = employeeModals[cardClickTargetId];
+    modal.innerHTML = employeeModals[cardClickedId];
     modalDiv.insertAdjacentElement('beforeend', modal);
     modalCloseBtn.addEventListener('click', closeModal);
-    getCurrentCardIndex(currentCardId);
-}
-
-const modalPrev = document.getElementById('modal-prev');
-const modalNext = document.getElementById('modal-next');
-
-function getCurrentCardIndex (currentCardId) {
-    const currentCards = [...(document.getElementsByClassName('card'))];
-    
-    let currentCardIndex = currentCards.findIndex(item => item.id === currentCardId);
-    modalBrowse(currentCardIndex.valueOf())
+    let currentCardIndex = currentCards.findIndex(item => item.id === cardClickedId);
+    modalBrowse(currentCardIndex);
 }
 
 function modalBrowse (currentCardIndex) {
-    let modalInfoContainer = document.querySelector('.modal-info-container');
+    const modalInfoContainer = document.querySelector('.modal-info-container');
     const currentCards = [...(document.getElementsByClassName('card'))];
-    let currentCardsIds = []
-    currentCards.forEach(item => currentCardsIds.push(item.id))
-    let currentModals = []
-    currentCardsIds.forEach(item => currentModals.push(employeeModals[item]))
-    //let currentCardIndexNum = currentCardIndex
-    let currentModal;
-    /* const previousCardIndex = currentCardIndex-1;
-    const nextCardIndex = currentCardIndex+1;
-    const previousCardModalIndex = parseInt(currentCards[previousCardIndex].id)
-    const nextCardModalIndex = parseInt(currentCards[nextCardIndex].id) */
+    const currentCardsIds = [];
+    currentCards.forEach(item => currentCardsIds.push(item.id));
+    const currentModals = [];
+    currentCardsIds.forEach(item => currentModals.push(employeeModals[item]));
     modalPrev.addEventListener('click', (e) => {
         modalInfoContainer.innerHTML = '';
         if (currentCardIndex === 0) {
@@ -127,7 +99,6 @@ function modalBrowse (currentCardIndex) {
             modalInfoContainer.innerHTML = currentModals[currentCardIndex-1];
             currentCardIndex -= 1
         }
-        console.log(currentCardIndex)
         e.stopPropagation();
     });
     modalNext.addEventListener('click', (e) => {
@@ -139,29 +110,19 @@ function modalBrowse (currentCardIndex) {
             modalInfoContainer.innerHTML = currentModals[currentCardIndex+1];
             currentCardIndex += 1
         }
-        console.log(currentCardIndex)
         e.stopPropagation();
     });
 }
 
 function closeModal(e) {
     const modalInfoContainer = document.querySelector('.modal-info-container');
-    
-    //modalInfoContainer.innerHTML =  '';
     modalInfoContainer.remove();
     modalContainer.style.display = 'none';
     e.stopPropagation();
 }
-
-async function fetchEmployeeData () {
-        const response = await fetch('https://randomuser.me/api/?results=12&nat=US&noinfo')
-        const json = await response.json()
-        .then((result) => {
-            generateCardsModals(result)
-        })
-}
-fetchEmployeeData();
-
+searchInput.addEventListener('keyup', () => {
+    search();
+})
 function search () {
     while (gallery.firstElementChild) {
         gallery.removeChild(gallery.firstElementChild)
@@ -192,7 +153,5 @@ function search () {
     }
 }
 
-searchInput.addEventListener('keyup', () => {
-    search();
-})
+
 
