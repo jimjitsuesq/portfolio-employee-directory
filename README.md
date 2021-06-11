7f30037fc8d620c34577e4bb3cdabb13e19ec22f
Jim Peters <jimjitsuesq@gmail.com>
2021-06-11 08:05:35 -0700

    Final version for submission
    All functionality working as expected.
    
    Minor CSS Changes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # public-api-employee-directory
  
+ *** CSS Style Changes ***
+Changed body background color and updated heading and other text color to match.
+Also added .no-results class to format "no results" message.
--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,31 @@
 const gallery = document.getElementById('gallery');
-const cardInfo = document.querySelector('.card-info-container');
-const modalDiv = document.querySelector('.modal');
 const modalContainer = document.querySelector('.modal-container');
-const modalPrev = document.getElementById('modal-prev');
-const modalNext = document.getElementById('modal-next');
-const searchInput = document.getElementById('search-input');
 const searchSubmit = document.getElementById('search-submit');
 const employeeCards = [];
 const employeeModals = [];
-let currentCardId;
 
 modalContainer.style.display = 'none';
 searchSubmit.style.display = 'none';
 
+/**
+ * Requests and waits for employee data from the server, then parses the
+ * results and calls generateHTML to work with the data.
+ */
 async function fetchEmployeeData () {
-    const response = await fetch('https://randomuser.me/api/?results=12&nat=US&noinfo')
+    const response = await fetch('https://randomuser.me/api/?results=12&nat=US&noinfo');
     await response.json()
     .then((result) => {
-        generateHTML(result)
+        generateHTML(result);
     })
 }
 fetchEmployeeData();
 
+/**
+ * Generates the employeeCards and employeeModals and collects them for later
+ * use. Inserts an index field into the card HTML to reference later. Calls 
+ * displayEmployeeCards for initial screen.
+ * @param {Object} data The data retrieved from randomuser.me
+ */
 function generateHTML (data) {
     data.results.map( (person, index) => {
         const card = document.createElement('div');
@@ -40,11 +44,11 @@ function generateHTML (data) {
         employeeCards.push(card);
     });
     data.results.map( (person) => {
-        const fixedPhone = person.cell.replace("-", " ");
+        const fixedPhone = person.cell.replace('-', ' ');
         const birthYear = person.dob.date.slice(0, 4);
         const birthMonth = person.dob.date.slice(5, 7);
         const birthDay = person.dob.date.slice(8, 10);
-        let html = 
+        let html =
             `<img class="modal-img" src="${person.picture.large}" alt="profile picture">
             <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
             <p class="modal-text">${person.email}</p>
@@ -58,19 +62,33 @@ function generateHTML (data) {
     displayemployeeCards();
 }
 
+/**
+ * Displays cards for all of the employees. Adds event listeners to display
+ * modals when clicked and pass along the id of the selected card.
+ */
 function displayemployeeCards () {
-    for (let i=0; i<employeeCards.length; i++) {
+    for (let i = 0; i < employeeCards.length; i++) {
         gallery.appendChild(employeeCards[i]);
         employeeCards[i].addEventListener('click', (e) => {
             cardClickedId = e.target.closest('.card').id;
-            displayModal(cardClickedId);
-            e.stopPropagation;   
+            displayModal(cardClickedId);  
         });
     }
 }
 
+/**
+ * Creates a div for the modal and injects the HTML for the selected card.
+ * Displays the modal along with the modal close button. Adds event listener
+ * to the close button that will call closeModal. Collects all of the currently 
+ * displayed cards into an array and then determines the index number of the 
+ * card clicked in that array. Calls modalBrowase to create toggle 
+ * functionality.
+ * @param {string} cardClickedId The id of the card that was clicked to display
+ * the modal.
+ */
 function displayModal (cardClickedId) {
     const currentCards = [...(document.getElementsByClassName('card'))];
+    const modalDiv = document.querySelector('.modal');
     const modalCloseBtn = document.getElementById('modal-close-btn');
     const modal = document.createElement('div');
     modal.id = cardClickedId;
@@ -83,75 +101,85 @@ function displayModal (cardClickedId) {
     modalBrowse(currentCardIndex);
 }
 
+/**
+ * Displays the previous and next navigation buttons to toggle through the
+ * displayed employees. Creates a new array containing the original index 
+ * values for each of the currently displayed cards and uses it to create
+ * a new array containing the modals for each displayed card. Adds event
+ * listeners to the navigation buttons to cycle through the HTML for the modals
+ * using the currentCardIndex variable.
+ * @param {number} currentCardIndex The original index value for the currently
+ * displayed modal.
+ */
 function modalBrowse (currentCardIndex) {
     const modalInfoContainer = document.querySelector('.modal-info-container');
+    const modalPrev = document.getElementById('modal-prev');
+    const modalNext = document.getElementById('modal-next');
     const currentCards = [...(document.getElementsByClassName('card'))];
     const currentCardsIds = [];
-    currentCards.forEach(item => currentCardsIds.push(item.id));
     const currentModals = [];
+    currentCards.forEach(item => currentCardsIds.push(item.id));
     currentCardsIds.forEach(item => currentModals.push(employeeModals[item]));
-    modalPrev.addEventListener('click', (e) => {
+    modalPrev.addEventListener('click', () => {
         modalInfoContainer.innerHTML = '';
         if (currentCardIndex === 0) {
             modalInfoContainer.innerHTML = currentModals[currentModals.length - 1];
-            currentCardIndex = currentModals.length - 1
+            currentCardIndex = currentModals.length - 1;
         } else {
-            modalInfoContainer.innerHTML = currentModals[currentCardIndex-1];
-            currentCardIndex -= 1
+            modalInfoContainer.innerHTML = currentModals[currentCardIndex - 1];
+            currentCardIndex -= 1;
         }
-        e.stopPropagation();
     });
-    modalNext.addEventListener('click', (e) => {
+    modalNext.addEventListener('click', () => {
         modalInfoContainer.innerHTML = '';
         if (currentCardIndex === currentModals.length - 1) {
             modalInfoContainer.innerHTML = currentModals[0];
-            currentCardIndex = 0
+            currentCardIndex = 0;
         } else {
-            modalInfoContainer.innerHTML = currentModals[currentCardIndex+1];
-            currentCardIndex += 1
+            modalInfoContainer.innerHTML = currentModals[currentCardIndex + 1];
+            currentCardIndex += 1;
         }
-        e.stopPropagation();
     });
 }
 
-function closeModal(e) {
+/**
+ * Hides the modal and removes the modalInfoContainer div.
+ */
+function closeModal() {
     const modalInfoContainer = document.querySelector('.modal-info-container');
     modalInfoContainer.remove();
     modalContainer.style.display = 'none';
-    e.stopPropagation();
 }
+
+/**
+ * Adds a keyup event listener to the searchInput element that fires the search
+ * function after each keystroke.
+ */
+const searchInput = document.getElementById('search-input');
 searchInput.addEventListener('keyup', () => {
     search();
-})
+});
+
+/**
+ * First removes all cards displayed on the page. Then compares the string in
+ * searchInput to the name of each employee, displaying it if there is a match.
+ * If there are no matches, a "no-results" message is displayed. 
+ */
 function search () {
     while (gallery.firstElementChild) {
         gallery.removeChild(gallery.firstElementChild)
     }
-    //searchModals = [];
-
-    for (let i=0; i<employeeCards.length; i++) {
-        if (employeeCards[i].children[0].childNodes[2].nextElementSibling.childNodes[1].textContent.toLowerCase().includes(searchInput.value.toLowerCase())) {
+    for (let i = 0; i < employeeCards.length; i++) {
+        const searchTarget = employeeCards[i].children[0].childNodes[2].nextElementSibling.childNodes[1];
+        if ((searchTarget.textContent.toLowerCase()).includes(searchInput.value.toLowerCase())) {
             gallery.appendChild(employeeCards[i]);
-            //searchModals.push(employeeModals[i]);
         }
-    } 
-
+    }
     if (searchInput.value.length === 0) {
-        for (let i=0; i< employeeCards.length; i++) {
-            gallery.appendChild(employeeCards[i]);
-        }
-        //searchModals = employeeModals;
-    } else {
-        
+        employeeCards.forEach(item => gallery.appendChild(item))
     }
-    const currentemployeeCards = document.getElementsByClassName('card');
-    if (currentemployeeCards.length === 0) {
-        gallery.insertAdjacentHTML("afterbegin", `<span class = "no-results">No Results found.</span>`);
+    const currentCards = document.getElementsByClassName('card');
+    if (currentCards.length === 0) {
+        gallery.insertAdjacentHTML('afterbegin', `<span class="no-results">No Results found.</span>`);
     } 
-    for (let i=0; i<currentemployeeCards.length; i++) {
-            currentemployeeCards[i].childNodes[1].id = [i];         
-    }
 }
-
-
-
